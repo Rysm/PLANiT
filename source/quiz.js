ef389ab93abbeb4e53b70d5163b4bb3e0c2001ab
Rysm <andyliwang95@gmail.com>
2015-10-24 15:22:48 -0700

    Syntaxio Fixio

--- source/quiz.js
+++ source/quiz.js
@@ -1,8 +1,7 @@
 var key = {{1,6,8,11,12,14,16,20,21,23,26,27},{1,7,11,16,20,21,22,23,24,25,26},{2,3,6,7,9,10,12,13,14,15,17,18,19,20,22,24,25,27},{3,4,5,10,13,15,17,18,19,20,22,27},{5,6,8,10,12,14,21,23,25,26},{1,3,4,10,11,14,16,23,24}};,
 var answers = {};
 var scores = {};
-var strings = {"Engineering","Art/Design", "Business/Enterprise", "Psychology", "Science", "Writing/Literature"
-"};
+var strings = {"Engineering","Art/Design", "Business/Enterprise", "Psychology", "Science", "Writing/Literature"};
 
 //answers input here with the elseif 
 function getScore(){
